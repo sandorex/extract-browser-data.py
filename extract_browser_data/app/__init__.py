32f0282872370246173503185b493de33fa19d82
Sandorex <rzhw3h@gmail.com>
2020-02-26 10:26:20 +0100

    Prevent importing app subpackage

---
+++ extract_browser_data/app/__init__.py
@@ -0,0 +1,4 @@
+'''This package contains an application not a library, it's not meant to be imported'''
+
+if __name__ != 'extract_browser_data.app':
+   raise Exception('This module is not meant to be imported')
