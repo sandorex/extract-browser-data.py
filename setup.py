72f4225fdfd6bd10b0f5d42a1f6f055b1bd43abf
Sandorex <rzhw3h@gmail.com>
2020-02-27 22:28:35 +0100

    Formatting and updating setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@ from setuptools import setup
 from extract_browser_data import VERSION
 
 setup(
-   version = VERSION,
-   name = 'extract-browser-data',
-   description = 'Python utility to extract cookies, history, bookmarks from browsers.',
-   url = 'https://github.com/sandorex/extract-browser-data.py',
-   license = 'Apache',
-   author = 'Sandorex',
-   author_email = 'rzhw3h@gmail.com',
-
-   entry_points = {
-      'console_scripts': [
-         'extract-browser-data=extract_browser_data.app.main:main',
-         'extract-chromium-data=extract_browser_data.app.main:main_chromium',
-         'extract-firefox-data=extract_browser_data.app.main:main_firefox',
-      ]
-   },
-
-   packages = [ 'extract_browser_data' ],
-   install_requires = [ x.strip() for x in open('requirements.txt').readlines() if x and not x.startswith('#') ],
-   python_requires = '>=3.6'
-)
+    version=VERSION,
+    name='extract-browser-data',
+    description=
+    'Python library and utility to extract cookies, history, bookmarks from browsers.',
+    url='https://github.com/sandorex/extract-browser-data.py',
+    license='Apache',
+    author='Sandorex',
+    author_email='rzhw3h@gmail.com',
+    entry_points={
+        'console_scripts': [
+            'extract-browser-data=extract_browser_data.app.main:main',
+            'extract-chromium-data=extract_browser_data.app.main:main_chromium',
+            'extract-firefox-data=extract_browser_data.app.main:main_firefox',
+        ]
+    },
+    packages=['extract_browser_data'],
+    install_requires=[
+        x.strip() for x in open('requirements.txt').readlines()
+        if x and not x.startswith('#')
+    ],
+    python_requires='>=3.6')
